42786d479500930dbfffc6c697d8a0056cde2181
jonathan <jonathan.developpeur@gmail.com>
2021-04-19 17:10:01 +0200

    pull_request#1_that_contains_the_3_features_of_sprint2

---
+++ src/main/java/com/openClassroomsProject/SafetyNetAlerts/model/Person.java
@@ -0,0 +1,22 @@
+package com.openClassroomsProject.SafetyNetAlerts.model;
+
+import lombok.Data;
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name="person")
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name="first_name")
+    private String firstName;
+    @Column(name="last_name")
+    private String lastName;
+    private String address;
+    private String city;
+    private int zip;
+    private int phone;
+    private String email;
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,27 @@
+DROP TABLE IF EXISTS person;
+DROP TABLE IF EXISTS firestation;
+DROP TABLE IF EXISTS medicalrecord;
+
+CREATE TABLE person (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(250),
+    last_name VARCHAR(250),
+    address VARCHAR(250),
+    city VARCHAR(250),
+    zip CHAR(5),
+    phone INT,
+    email VARCHAR(250)
+);
+CREATE TABLE firestation (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    address VARCHAR(250),
+    station TINYINT
+);
+CREATE TABLE medicalrecord (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(250),
+    last_name VARCHAR(250),
+    birthdate DATE,
+    medications VARCHAR(250),
+    allergies VARCHAR(250)
+);
